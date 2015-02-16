5ffe6fb5e702d77ac103baa0c7405616f405aa3c
Nathan Shayefar <nshayefar@twitter.com>
2015-02-16 08:54:02 -0800

    Added API/YelpClient from Codepath Yelp starter project.

---
+++ 2_Yelp/Business.swift
@@ -0,0 +1,13 @@
+//
+//  Business.swift
+//  2_Yelp
+//
+//  Created by Nathan Shayefar on 2/16/15.
+//  Copyright (c) 2015 Nathan Shayefar. All rights reserved.
+//
+
+import UIKit
+
+class Business: NSObject {
+   
+}
---
+++ 2_Yelp/YelpClient.swift
@@ -0,0 +1,35 @@
+//
+//  YelpClient.swift
+//  2_Yelp
+//
+//  Created by Nathan Shayefar on 2/16/15.
+//  Copyright (c) 2015 Nathan Shayefar. All rights reserved.
+//
+
+import UIKit
+
+// Source: Codepath Yelp starter project (https://github.com/codepath/ios_yelp_swift)
+class YelpClient: BDBOAuth1RequestOperationManager {
+    var accessToken: String!
+    var accessSecret: String!
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    init(consumerKey key: String!, consumerSecret secret: String!, accessToken: String!, accessSecret: String!) {
+        self.accessToken = accessToken
+        self.accessSecret = accessSecret
+        var baseUrl = NSURL(string: "http://api.yelp.com/v2/")
+        super.init(baseURL: baseUrl, consumerKey: key, consumerSecret: secret);
+        
+        var token = BDBOAuthToken(token: accessToken, secret: accessSecret, expiration: nil)
+        self.requestSerializer.saveAccessToken(token)
+    }
+    
+    func searchWithTerm(term: String, success: (AFHTTPRequestOperation!, AnyObject!) -> Void, failure: (AFHTTPRequestOperation!, NSError!) -> Void) -> AFHTTPRequestOperation! {
+        // For additional parameters, see http://www.yelp.com/developers/documentation/v2/search_api
+        var parameters = ["term": term, "location": "San Francisco"]
+        return self.GET("search", parameters: parameters, success: success, failure: failure)
+    }
+}
