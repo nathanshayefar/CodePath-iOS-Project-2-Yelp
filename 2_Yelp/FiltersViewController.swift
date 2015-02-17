6fdae50cbde7387e367822e4da07c6b75eec0ffe
Nathan Shayefar <nshayefar@twitter.com>
2015-02-16 17:35:42 -0800

    Added skeleton implementation of SwitchCell and SwitchCellDelegate.

--- 2_Yelp/FiltersViewController.swift
+++ 2_Yelp/FiltersViewController.swift
@@ -12,20 +12,28 @@ protocol FiltersViewControllerDelegate : class {
     func didChangeFilters(filtersViewController: FiltersViewController, filters: NSDictionary)
 }
 
-class FiltersViewController: UIViewController {
+class FiltersViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    
     weak var delegate: FiltersViewControllerDelegate?
     var filters = NSDictionary()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        tableView.dataSource = self
+//        tableView.delegate = self
+        
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .Plain, target: self, action: "onCancelButton")
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Apply", style: .Plain, target: self, action: "onApplyButton")
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
     
     func onCancelButton() {
---
+++ 2_Yelp/SwitchCell.swift
@@ -0,0 +1,43 @@
+//
+//  SwitchCell.swift
+//  2_Yelp
+//
+//  Created by Nathan Shayefar on 2/16/15.
+//  Copyright (c) 2015 Nathan Shayefar. All rights reserved.
+//
+
+import UIKit
+
+protocol SwitchCellDelegate : class {
+    func didUpdateValue(switchCell: SwitchCell, value: Bool)
+}
+
+class SwitchCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var toggleSwitch: UISwitch!
+    
+    weak var delegate: SwitchCellDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setSwitchValueAnimated(value: Bool, animated: Bool) {
+        toggleSwitch.setOn(value, animated: animated)
+    }
+    
+    func setSwitchValue(value: Bool) {
+        setSwitchValueAnimated(value, animated: false)
+    }
+    
+    @IBAction func switchValueChanged(sender: AnyObject) {
+        delegate!.didUpdateValue(self, value: toggleSwitch.on)
+    }
+}
